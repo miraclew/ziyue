72b490423ba48509228531f2abe528ac39c66562
miraclew <miraclew@gmail.com>
2013-12-19 11:20:30 +0800

    bbb

--- admin/app/models/course.rb
+++ admin/app/models/course.rb
@@ -1,17 +1,23 @@
 class Course
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :title_cn, type: String
-  field :cat
+  field :cat, type: Integer
   field :subject, type: String
   field :author
   field :desc
   field :duration
-  field :is_pub
-  field :on_sale
-  field :on_sale_at
-  field :released_at
+  field :is_pub, type: Mongoid::Boolean
+  field :on_sale, type: Mongoid::Boolean
+  field :on_sale_at, type: Time
+  field :released_at, type: Time
   field :software
-  field :vip_expired_at
-  field :created_at
+  field :vip_expired_at, type: Time
+
+  index :title => 1
+  index :cat => 1
+
+  has_many :chapters
 end
--- blog/app/controllers/posts_controller.rb
+++
@@ -1,7 +0,0 @@
-class PostsController < ApplicationController
-  
-  def new 
-    
-  end
-  
-end
